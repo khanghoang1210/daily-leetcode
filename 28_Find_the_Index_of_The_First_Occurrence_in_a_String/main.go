9074dcc3f14e972c241c5f5989723989a0b0300d
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2024-07-24 22:12:11 +0700

    28. Find the index of the first occurence in a string (accepted)

--- 28_Find_the_Index_of_The_First_Occurrence_in_a_String/main.go
+++ 28_Find_the_Index_of_The_First_Occurrence_in_a_String/main.go
@@ -3,21 +3,33 @@ package main
 import "fmt"
 
 // accept 67/83 test cases
+// func strStr(haystack string, needle string) int {
+//     index := 0
+//     for i := 0; i <= len(haystack)-1; i++ {
+//         if haystack[i] == needle[index]{
+//             if index >= len(needle)-1 {
+//                 return i - index
+
+//             } else{
+//                 index++
+//             }
+
+//         } else {
+//             index = 0
+//             i = i
+//         }
+
+//     }
+//     return -1
+// }
+
 func strStr(haystack string, needle string) int {
-    index := 0
-    for i := 0; i <= len(haystack)-1; i++ {
-        if haystack[i] == needle[index]{
-            if index >= len(needle)-1 {
-                return i - index
-
-            } else{
-                index++
-            }
-
-        } else {
-            index = 0
+    for i := 0; i < len(haystack); i++ {
+        if len(needle) + i > len(haystack) {
+            return -1
+        } else if haystack[i:len(needle)+i] == needle {
+            return i
         }
-
     }
     return -1
 }
@@ -25,7 +37,7 @@ func strStr(haystack string, needle string) int {
 func main() {
 	// Test cases
 	haystack := "mississippi"
-	needle := "issp"
+	needle := "issip"
 	// Running test cases
 
 	fmt.Printf("Input: haystack = %s\n", haystack)
