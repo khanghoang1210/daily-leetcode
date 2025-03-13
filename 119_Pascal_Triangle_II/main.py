bfcc3769158925c3f1b4771241e13523629a4034
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2025-03-13 18:04:16 +0700

    119. Pascal's Triangle II

---
+++ 119_Pascal_Triangle_II/main.py
@@ -0,0 +1,16 @@
+def getRow(rowIndex: int) :
+    arr = [1]
+    for i in range(0, rowIndex):
+        if i == rowIndex:
+            return arr
+        else:
+            tmp = arr
+            arr = []
+            for j in range(0, len(tmp) - 1):
+                arr.append(tmp[j] + tmp[j+1])
+            arr.append(1)
+            arr.reverse()
+            arr.append(1)
+    return arr
+            
+print(getRow(6)) # [1, 6, 15, 20, 15, 6, 1]
